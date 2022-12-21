b104db9018ffc56122a9b211ab48cc1e6314dc4e
William Castro <wicastro98@gmail.com>
2022-12-21 18:10:36 -0500

    feat:✨Add login/create modal layout

--- resources/views/layouts/topbar_front.blade.php
+++ resources/views/layouts/topbar_front.blade.php
@@ -10,32 +10,252 @@
                 </div>
             </div>
             <div class="col-lg-8 col-md-8 col-6">
-            <div class="col-lg-8 col-md-8 col-6">
-                        <div class="header-right-side text-right">
-                            <div class="header-left-search d-none d-md-block">
-                                <a href="/buscador">
-                                    <i class="icon-magnifier"></i>
-                                </a>
-                            </div>
-                            <div class="header-right-items d-none d-md-block">
-                                <a href="{{route('admin.login')}}" >
-                                    <i class="icon-user"></i>
-                                </a>
-                            </div>
-                            <div class="header-right-items d-none d-md-block">
-                                <a href="{{ url('/') }}" class="header-cart">
-                                    <i class="icon-home"></i>
-                                </a>
-                            </div>
+                <div class="col-lg-8 col-md-8 col-6">
+                    <div class="header-right-side text-right">
+                        <div class="header-left-search d-none d-md-block">
+                            <a href="/buscador">
+                                <i class="icon-magnifier"></i>
+                            </a>
+                        </div>
+                        <div class="header-right-items d-none d-md-block">
+                            <a href="{{ route('admin.login') }}">
+                                <i class="icon-user"></i>
+                            </a>
+                        </div>
+                        <div class="header-right-items d-none d-md-block">
+                            <a href="{{ url('/') }}" class="header-cart">
+                                <i class="icon-home"></i>
+                            </a>
+                        </div>
+                        <div class="header-right-items d-none d-md-block">
+                            <button type="button" class="btn btn-dark rounded d-none d-lg-block mr-1" data-toggle="modal" data-target="#loginModal">
+                                Acceder
+                            </button>
+                        </div>
 
-                            <div class="header-right-items d-block d-md-none">
-                                <a href="javascript:void(0)" class="search-icon" id="search-overlay-trigger">
-                                    <i class="icon-magnifier"></i>
-                                </a>
-                            </div>
+                        <div class="header-right-items d-block d-md-none">
+                            <a href="javascript:void(0)" class="search-icon" id="search-overlay-trigger">
+                                <i class="icon-magnifier"></i>
+                            </a>
                         </div>
                     </div>
-        </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+
+<!-- Login modal -->
+<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-sm modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <div class="d-flex flex-column flex-md-row">
+                    <div class="modal-image">
+                        <img src="{{asset('assets/images/logos/logo.png')}}" class="card-img-top" alt="...">
+                    </div>
+                    <div class="overflow-hidden modal-form">
+                        <div class="modal-header">
+                            <h3 class="col-12 modal-title text-center">Iniciar sesión
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </h3>
+                        </div>
+                        @error('email')
+                            <div class="alert alert-danger" role="alert">{{ $message }}</div>
+                        @enderror
+                        <div class="p-2">
+                            <form class="form-horizontal" method="POST" action="{{ route('admin.login') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <input name="email" type="text" class="form-control @error('email') is-invalid @enderror autofill-input" @if (old('email')) value="{{ old('email') }}" @endif id="login-email" placeholder="" autocomplete="email" required>
+                                    <label for="email" id="email-label">Correo electrónico</label>
+                                </div>
+
+                                <div class="form-group">
+                                    <input type="password" name="password" class="form-control @error('password') is-invalid @enderror autofill-input" id="login-password" placeholder="" required>
+                                    <label for="password" id="password-label">Ingresa tu contraseña</label>
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+
+                                <div class="mt-4 mb-4 text-center">
+                                    <a href="{{ route('admin.password.request') }}" class="text-muted"><i class="mdi mdi-lock mr-1"></i>
+                                        ¿Ha olvidado su contraseña?</a>
+                                </div>
+
+                                <div id="modal-login-footer">
+                                    <button type="button" class="btn btn-outline-primary" data-dismiss="modal" data-toggle="modal" data-target="#createModal">Crear cuenta</button>
+                                    <button type="submit" class="btn btn-primary">Ingresar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End login modal -->
+
+<!-- Create account modal -->
+<div class="modal fade" id="createModal" tabindex="-1" role="dialog" aria-labelledby="createModal" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <div class="d-flex flex-column flex-md-row">
+                    <div class="modal-image">
+                        <img src="{{asset('assets/images/logos/logo.png')}}" class="card-img-top" alt="...">
+                    </div>
+
+                    <div class="overflow-hidden modal-form">
+                        <div class="modal-header">
+                            <h3 class="col-12 modal-title text-center">Crear una cuenta
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </h3>
+                        </div>
+                        <div class="p-2">
+                            <form method="POST" action="{{ route('admin.register.create') }}" enctype="multipart/form-data">
+                                @csrf
+                                <div class="form-group">
+                                    <input type="text" class="form-control @error('email') is-invalid @enderror autofill-input" value="{{ old('email') }}" id="create-email" name="email" placeholder="" required>
+                                    <label for="email" id="email-label">Correo electrónico</label>
+                                    @error('email')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+    
+                                <div class="form-group">
+                                    <input type="text" class="form-control @error('name') is-invalid @enderror autofill-input" value="{{ old('name') }}" required name="name" id="name" placeholder="">
+                                    <label for="name" id="name-label">Nombre</label>
+                                    @error('name')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+    
+                                <div class="form-group">
+                                    <input type="password" class="form-control @error('password') is-invalid @enderror autofill-input" name="password" required id="create-password" placeholder="">
+                                    <label for="password" id="password-label">Contraseña</label>
+                                    @error('password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+    
+                                <div class="form-group">
+                                    <input id="password-confirm" type="password" name="password_confirmation" class="form-control @error('password') is-invalid @enderror autofill-input" name="password" required placeholder="">
+                                    <label for="password-confirm" id="password-confirm-label">Confirmación de contraseña</label>
+                                </div>
+    
+                                <div id="modal-create-footer">
+                                    <button type="button" class="btn btn-outline-primary" data-dismiss="modal" data-toggle="modal" data-target="#loginModal">Prefiero iniciar sesión</button>
+                                    <button type="submit" class="btn btn-primary">Registrar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End create account modal -->
+
+<style>
+    .modal-image {
+        width: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-top-right-radius: 0;
+        border-bottom-left-radius: calc(0.25rem - 1px);
+    }
+
+    .modal-form {
+        width: 50%;
+        border-top-right-radius: 0;
+        border-bottom-left-radius: calc(0.25rem - 1px);
+    }
+
+    .form-group {
+        position: relative;
+        margin-bottom: 25px;
+    }
+
+    .autofill-input:-webkit-autofill,
+    .autofill-input:-webkit-autofill:hover,
+    .autofill-input:-webkit-autofill:focus,
+    .autofill-input:-webkit-autofill:active {
+        -webkit-box-shadow: 0 0 0 30px white inset !important;
+    }
+
+    #email-label,
+    #name-label,
+    #password-label,
+    #password-confirm-label {
+        position: absolute;
+        top: 6px;
+        left: 13px;
+        color: #83879a;
+        font-weight: lighter;
+        cursor: text;
+        transition: 0.2s ease-in-out;
+        pointer-events: none;
+    }
+
+    #login-email:focus~#email-label,
+    #login-email:not(:focus):valid~#email-label,
+    #create-email:focus~#email-label,
+    #create-email:not(:focus):valid~#email-label,
+    #name:focus~#name-label,
+    #name:not(:focus):valid~#name-label,
+    #login-password:focus~#password-label,
+    #login-password:not(:focus):valid~#password-label,
+    #create-password:focus~#password-label,
+    #create-password:not(:focus):valid~#password-label,
+    #password-confirm:focus~#password-confirm-label,
+    #password-confirm:not(:focus):valid~#password-confirm-label {
+        color: #000000;
+        font-weight: bolder;
+        font-size: 11px;
+        padding-left: 4px;
+        padding-right: 4px;
+        top: -10px;
+        left: 10px;
+        background-color: #fff;
+        pointer-events: none;
+    }
+
+    #modal-login-footer {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding-top: 15px;
+        border-top: 1px solid #eff2f7;
+        border-bottom-right-radius: calc(0.3rem - 1px);
+        border-bottom-left-radius: calc(0.3rem - 1px);
+    }
+
+    #modal-create-footer {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        margin-top: 40px;
+        padding-top: 15px;
+        border-top: 1px solid #eff2f7;
+        border-bottom-right-radius: calc(0.3rem - 1px);
+        border-bottom-left-radius: calc(0.3rem - 1px);
+    }
+</style>
