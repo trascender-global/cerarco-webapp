9ef652eb201b495b77315a917ab7965c4e8cc4d1
William Castro <wicastro98@gmail.com>
2023-01-11 20:40:41 -0500

    feat:💄Add individual forum in modal

--- resources/views/front/page/foro/discusion.blade.php
+++ resources/views/front/page/foro/discusion.blade.php
@@ -4,66 +4,163 @@
 
 @section('content')
 
-    @component('common-components.breadcrumb-front')
-        @slot('title') Foro @endslot
+@component('common-components.breadcrumb-front')
+    @slot('title') Foro @endslot
         @slot('seccion') Foro @endslot
-    @endcomponent
+        @endcomponent
 
-    <div class="site-wrapper-reveal border-bottom">
-        <div class="blog-page-wrapper section-space--pt_60 section-space--pb_120">
-            <div class="container">
-                <div class="row flex-column flex-md-row">
-                    <div>
-                        <span class="order-title ml-1 ml-md-5">Ordenar por:</span>
-                        <button type="button" class="most-comments-btn d-block d-md-inline">
-                            Fichas más comentadas
-                        </button>
-                        <button type="button" class="new-comments-btn d-block d-md-inline">
-                            Fichas con comentarios recientes
-                        </button>
+        <div class="site-wrapper-reveal border-bottom">
+            <div class="blog-page-wrapper section-space--pt_60 section-space--pb_120">
+                <div class="container">
+                    <div class="row flex-column flex-md-row">
+                        <div>
+                            <span class="order-title-19 ml-1 ml-md-5">Ordenar por:</span>
+                            <button type="button" class="solid-blue-btn filter-comments-19 d-block d-md-inline">
+                                Fichas más comentadas
+                            </button>
+                            <button type="button" class="outlined-blue-btn filter-comments-19 d-block d-md-inline">
+                                Fichas con comentarios recientes
+                            </button>
+                        </div>
                     </div>
-                </div>
 
-                <!-- Comment card -->
-                <div class="card flex-column flex-md-row mt-2 mt-md-4 ml-1 ml-md-4">
-                    <div class="card-description">
-                        <p class="card-p-primary">CCsin1</p>
-                        <p class="card-p-secondary">Copa de pedestal cilindrico</p>
-                        <img class="card-description-img" src="..." alt="Imagen de ficha">
-                        <button type="button" class="card-description-btn d-block mt-2 mt-md-3">
-                            Ver ficha
-                        </button>
-                    </div>
-                    <div class="card-body">
-                        <button type="button" class="interact-btn d-flex justify-content-center align-items-center">
-                            <img src="{{ asset('assets/svg/like-icon.svg') }}" alt="Like icon" class="p-1">
-                            <span class="number-count">58</span>
-                        </button>
-                        <p class="description mt-2 mt-md-3">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <hr class="horizontal-line">
-                        <div class="row flex-column flex-md-row">
-                            <div class="col-8 description description-title">
-                                User name
+                    <!-- Comment card -->
+                    <div class="card flex-column flex-md-row mt-2 mt-md-4 ml-1 ml-md-4">
+                        <div class="card-description">
+                            <p class="card-p-primary">CCsin1</p>
+                            <p class="card-p-secondary">Copa de pedestal cilindrico</p>
+                            <img class="card-description-img" src="..." alt="Imagen de ficha">
+                            <button type="button" class="card-description-btn d-block mt-2 mt-md-3">
+                                Ver ficha
+                            </button>
+                        </div>
+                        <div class="card-body">
+                            <div class="d-flex justify-content-center justify-content-md-start align-items-center">
+                                <button type="button" class="interact-btn d-flex justify-content-center align-items-center">
+                                    <img src="{{ asset('assets/svg/like-icon.svg') }}" alt="Like icon" class="p-1">
+                                    <span class="number-count">58</span>
+                                </button>
+                                <button type="button" class="interact-btn d-flex justify-content-center align-items-center" data-toggle="modal" data-target=".forum-modal">
+                                    <img src="{{ asset('assets/svg/share-arrow.svg') }}" width="24" height="24" alt="Like icon" class="p-1">
+                                    <span class="reply-title">Responder</span>
+                                </button>
                             </div>
-                            <div class="col-5 col-md-3">
-                                <button type="button" class="date-comment-btn d-flex justify-content-center align-items-center">
-                                    <img src="{{ asset('assets/svg/clock-icon.svg') }}" alt="Message icon" class="p-1">
+                            <p class="description mt-2 mt-md-3">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <hr class="horizontal-line">
+                            <div class="row flex-column flex-md-row">
+                                <div class="col-8 description description-title ml-2 ml-md-0">
+                                    User name
+                                </div>
+                                <div class="col-7 col-md-3">
+                                    <button type="button" class="date-comment-btn d-flex justify-content-center align-items-center">
+                                        <img src="{{ asset('assets/svg/clock-icon.svg') }}" alt="Message icon" class="p-1">
                                         Hace 1 día
-                                </button>
+                                    </button>
+                                </div>
+                                <div class="col-1">
+                                    <button type="button" class="messages-btn d-flex justify-content-center align-items-center">
+                                        <img src="{{ asset('assets/svg/message-grey-icon.svg') }}" alt="Message icon" class="p-1">
+                                        5
+                                    </button>
+                                </div>
                             </div>
-                            <div class="col-1">
-                                <button type="button" class="messages-btn d-flex justify-content-center align-items-center">
-                                <img src="{{ asset('assets/svg/message-grey-icon.svg') }}" alt="Message icon" class="p-1">
-                                    5
-                                </button>
+                        </div>
+                    </div>
+                    <!-- End comment card -->
+                </div>
+            </div>
+
+            <!-- Forum modal -->
+            <div class="modal fade forum-modal" tabindex="-1" role="dialog" aria-labelledby="forumModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-container" role="document">
+                    <div class="modal-content">
+                        <div class="modal-top">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="container-fluid">
+                                <div class="row flex-column flex-md-row">
+                                    <div class="col-2 d-none d-md-flex justify-content-end align-items-start">
+                                        <button type="button" class="interact-btn like-vertical d-none d-md-block mt-md-2">
+                                            <p class="mb-0">
+                                                <img src="{{ asset('assets/svg/like-icon.svg') }}" alt="Like icon">
+                                            </p>
+                                            <p class="number-count">58</p>
+                                        </button>
+                                    </div>
+                                    <div class="col">
+                                        <div class="row flex-column flex-md-row">
+                                            <div class="col">
+                                                <p class="comment-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ipsum urna, blandit eu felis eu, dictum pretium ipsum.</p>
+                                            </div>
+                                        </div>
+                                        <div class="row flex-column flex-md-row">
+                                            <div class="col-5 col-md-3 mt-3 mt-md-0 ml-2 ml-md-0">
+                                                <p class="user-name-title-20">User name</p>
+                                            </div>
+                                            <div class="col-6 col-md-3">
+                                                <button type="button" class="date-comment-btn d-flex justify-content-center align-items-center">
+                                                    <img src="{{ asset('assets/svg/clock-icon.svg') }}" alt="Message icon" class="p-0 p-md-1">
+                                                    Hace 1 día
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div class="row flex-column flex-md-row mt-5 mt-md-0 mb-4 mb-md-0">
+                                            <div class="col forum-input-container">
+                                                <textarea class="forum-input" placeholder="Escribe tu aporte"></textarea>
+                                                <div class="row flex-column align-items-end">
+                                                    <p>
+                                                        <button type="button" class="solid-blue-btn send-btn">
+                                                            Publicar
+                                                        </button>
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="row flex-column flex-md-row mt-2 mt-md-3 mb-2 mb-md-5">
+                                            <div class="col">
+                                                <span class="order-title-15">Ordenar por:</span>
+                                                <button type="button" class="solid-blue-btn filter-comments-15 d-block d-md-inline">
+                                                    Más votados
+                                                </button>
+                                                <button type="button" class="outlined-blue-btn filter-comments-15 d-block d-md-inline">
+                                                    Nuevos
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div class="row flex-column flex-md-row">
+                                            <div class="comment-card ml-2 ml-md-0 mb-2 mb-md-3">
+                                                <div class="row flex-column flex-md-row pl-1 pl-md-3">
+                                                    <div class="col">
+                                                        <p class="user-name-title-17 mb-2">User name</p>
+                                                        <p class="post-date-title-17 mb-2 mb-md-4">Hace 1 día</p>
+                                                    </div>
+                                                    <div class="col">
+                                                        <div class="row flex-column flex-md-row justify-content-center justify-content-md-end mr-2 mr-md-3">
+                                                            <button type="button" class="interact-btn d-flex justify-content-start justify-content-md-center align-items-center ml-2 ml-md-0">
+                                                                <img src="{{ asset('assets/svg/like-icon.svg') }}" alt="Like icon" class="p-1">
+                                                                <span class="number-count">58</span>
+                                                            </button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row flex-column flex-md-row pl-1 pl-md-3">
+                                                    <div class="col">
+                                                        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ipsum urna, blandit eu felis eu, dictum pretium ipsum. Nam vel velit faucibus, bibendum nibh a, iaculis mi. Donec velit purus, posuere eu magna mattis, varius efficitur justo.</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <!-- End comment card -->
             </div>
         </div>
-    </div>
 
 @endsection
 
@@ -71,15 +168,35 @@
 
     <style>
         @media (max-width: 768px) {
-            .most-comments-btn {
+            .solid-blue-btn {
                 margin: 10px 0px 10px 0px !important;
             }
-            .new-comments-btn {
+            .outlined-blue-btn {
                 margin: 10px 0px 10px 0px !important;
+            }
+            .modal-dialog-container {
+                max-width: 85% !important;
+                margin: 1.75rem auto !important;
+            }
+            .comment-title {
+                font-size: 23px !important;
             }
             .card-description {
                 width: 100% !important;
                 border-radius: 13px !important;
+            }
+            .forum-input-container {
+                margin: 0px 0px 0px -15px !important;
+            }
+            .forum-input {
+                width: 100% !important;
+                margin: 5px 0px 0px 0px !important;
+            }
+        }
+
+        @media (min-width: 992px) {
+            .modal-dialog-container {
+                max-width: 85% !important;
             }
         }
 
@@ -87,48 +204,49 @@
             border-radius: 13px !important;
         }
 
-        .most-comments-btn {
+        .solid-blue-btn {
             background-color: #3366CC;
             border: 1px solid #3366CC;
-            border-radius: 16px;
             text-align: center;
-            font-size: 19px;
-            color: #FFFFFF;
             margin: 0px 10px 0px 15px;
+            color: #FFFFFF;
         }
 
-        .most-comments-btn:hover {
+        .solid-blue-btn:hover {
             background-color: #3366CC60;
             border: 1px solid #3366CC60;
         }
 
-        .most-comments-btn:active {
+        .solid-blue-btn:active {
             background-color: #3366CC;
             border: 1px solid #3366CC90;
         }
 
-        .new-comments-btn {
+        .outlined-blue-btn {
             background-color: #E4EDFF;
             border: 1px solid #3366CC;
-            border-radius: 16px;
             text-align: center;
-            font-size: 19px;
             color: #3366CC;
         }
 
-        .new-comments-btn:hover {
+        .outlined-blue-btn:hover {
             background-color: #E4EDFF60;
             border: 1px solid #3366CC60;
         }
-
-        .new-comments-btn:active {
+        
+        .outlined-blue-btn:active {
             background-color: #E4EDFF;
             border: 1px solid #3366CC90;
         }
-        
-        .order-title {
+
+        .order-title-19 {
             font-size: 19px;
             color: #113B8F;
+        }
+
+        .filter-comments-19 {
+            border-radius: 16px;
+            font-size: 19px;
         }
 
         .card-description {
@@ -198,6 +316,19 @@
             color: #3366CC;
         }
 
+        .like-vertical {
+            background: #E4EDFF;
+            border: 1px solid #E4EDFF;
+            border-radius: 5px;
+        }
+
+        .reply-title {
+            font-size: 13px;
+            font-weight: 600;
+            color: #3366CC;
+            margin: 0px 4px 0px 4px;
+        }
+
         .horizontal-line {
             width: 100%;
             border: 2px solid #F1ECEC;
@@ -217,7 +348,7 @@
             background-color: #FFFFFF;
             border: none;
             text-align: center;
-            font-size: 13px;
+            font-size: 20px;
             color: #A3A3A3;
             cursor: text !important;
         }
@@ -228,6 +359,77 @@
             text-align: center;
             font-size: 13px;
             color: #A3A3A3;
+        }
+
+        .forum-modal {
+            background-color: #113B8F85;
+            opacity: 1;
+        }
+
+        .modal-container {
+            width: 95% !important;
+            height: 70% !important;
+        }
+
+        .modal-top {
+            display: flex;
+            justify-content: flex-end;
+            padding: 1rem 1rem;
+            border-top-left-radius: calc(.3rem - 1px);
+            border-top-right-radius: calc(.3rem - 1px);
+        }
+
+        .comment-title {
+            font-size: 32px;
+            font-weight: 600;
+            color: #67696E;
+        }
+
+        .user-name-title-20 {
+            font-size: 20px;
+            font-weight: 600;
+            color: #113B8F;
+        }
+
+        .user-name-title-17 {
+            font-size: 17px;
+            font-weight: 600;
+            color: #113B8F;
+        }
+
+        .post-date-title-17 {
+            font-size: 13px;
+            font-weight: 600;
+            color: #82858B;
+        }
+
+        .forum-input {
+            background-color: #E4EDFF;
+            border: none;
+            border-radius: 13px;
+            width: 100%;
+            padding: 10px;
+            margin: 37px 0px 0px 0px;
+        }
+
+        .send-btn {
+            border-radius: 8px;
+            font-size: 20px;
+        }
+
+        .order-title-15 {
+            font-size: 15px;
+            color: #113B8F;
+        }
+
+        .filter-comments-15 {
+            border-radius: 13px;
+            font-size: 15px;
+        }
+
+        .comment-card {
+            border: 3px solid #E2E2E2;
+            border-radius: 13px;
         }
     </style>
 
